e14462fe81253a1887454925b3e7306536a75547
Benjamin Yu <ben@previewme.co>
2022-09-03 16:36:03 +1200

    added  colours

--- components/Replacer.tsx
+++ components/Replacer.tsx
@@ -1,6 +1,22 @@
 import { useState } from "react";
 
 const Replacer = function () {
+  const ignored: string[] = [];
+  ignored.push("06a441bf-a2da-4e02-b75a-4edf33458ea6");
+  ignored.push("fa8ff9ae-c308-47c7-94f5-75fb6953c789");
+  ignored.push("1ab20075-3153-4fcc-9eb2-a019c0048952");
+
+  const colours: string[] = [
+    "#9ca3af",
+    "#f87171",
+    "#fbbf24",
+    "#a3e635",
+    "#34d399",
+    "#22d3ee",
+    "#818cf8",
+    "#e879f9",
+    "#fb7185",
+  ];
   const [currentString, setCurrentString] = useState("");
   const regex =
     /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
@@ -15,7 +31,9 @@ const Replacer = function () {
     const newLineIndex: number[] = [];
 
     for (let i = 0; i < currentString.length - uuidLength + 1; i++) {
-      if (currentString.substring(i, i + uuidLength).match(regex)) {
+      const uuidString = currentString.substring(i, i + uuidLength);
+      if (ignored.includes(uuidString)) continue;
+      if (uuidString.match(regex)) {
         matches.push(i);
       }
       if (currentString.charCodeAt(i) === 10) {
@@ -24,6 +42,7 @@ const Replacer = function () {
     }
 
     const renderedContent = [];
+    let colourIndex = 0;
     let current = 0;
     let a = 0;
     let b = 0;
@@ -43,8 +62,12 @@ const Replacer = function () {
       }
 
       if (nextMatch === current) {
+        colourIndex = (colourIndex + 1) % colours.length;
         renderedContent.push(
-          <span className="bg-green-400">
+          <span
+            className="bg-green-400"
+            style={{ backgroundColor: colours[colourIndex] }}
+          >
             {currentString.substring(current, current + 36)}
           </span>
         );
@@ -68,6 +91,12 @@ const Replacer = function () {
 
   return (
     <div className="w-5/6">
+      <div>
+        Ignored{" "}
+        {ignored.map((e) => (
+          <ul>{e}</ul>
+        ))}
+      </div>
       <div>
         <textarea
           onChange={(e) => onChange(e.target.value)}
